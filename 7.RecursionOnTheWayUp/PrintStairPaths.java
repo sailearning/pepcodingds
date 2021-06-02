9e66f0595e166e278b887f4037475410c705ee45
sailearning <83111309+sailearning@users.noreply.github.com>
2021-06-02 16:45:42 +0530

    Update PrintStairPaths.java

--- 7.RecursionOnTheWayUp/PrintStairPaths.java
+++ 7.RecursionOnTheWayUp/PrintStairPaths.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class PrintStairPaths {
+public class Main{
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
