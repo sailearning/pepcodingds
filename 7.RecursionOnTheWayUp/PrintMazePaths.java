2f88b85419c4511dd9f7a5ac9ef3efad8140ae3d
sailearning <83111309+sailearning@users.noreply.github.com>
2021-06-03 11:07:10 +0530

    Create PrintMazePaths.java

---
+++ 7.RecursionOnTheWayUp/PrintMazePaths.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int dr = scn.nextInt();
+        int dc = scn.nextInt();
+        printMazePaths(0,0,dr-1,dc-1,"");
+    }
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+        if(sr==dr && sc == dc ){
+            System.out.println(psf);
+            return;
+        }
+        if(sr>dr || sc>dc){
+            return;
+        }
+
+        //For Vertical Movement
+        printMazePaths(sr+1,sc,dr,dc,psf+"v");
+
+        //For Horizontal Movement
+        printMazePaths(sr,sc+1,dr,dc,psf+"h");
+
+    }
+}
